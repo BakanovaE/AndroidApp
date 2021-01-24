ed06ebcd1aeff2de9e84f7dc33f416b9d3202235
Ekaterina Bakanova <k125598@yandex.ru>
2021-01-24 12:11:41 +0300

    МView elements reverences

---
+++ app/src/main/java/com/example/androidapp/AuthActivity.java
@@ -0,0 +1,47 @@
+package com.example.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private EditText mLogin;
+    private EditText mPassword;
+
+    private Button mEnter;
+    private Button mRegister;
+
+    private View.OnClickListener mOnEnterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //todo Обработка нажатие по  кнопке
+        }
+    };
+    private View.OnClickListener mOnRegisterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //todo Обработка нажатие по  кнопке
+        }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_auth);
+
+        mLogin = findViewById(R.id.etLogin);
+        mPassword = findViewById(R.id.etPassword);
+
+        mEnter = findViewById(R.id.buttonEnter);
+        mRegister = findViewById(R.id.buttonRegister);
+
+        mEnter.setOnClickListener(mOnEnterClickListener);
+        mRegister.setOnClickListener(mOnRegisterClickListener);
+    }
+}
